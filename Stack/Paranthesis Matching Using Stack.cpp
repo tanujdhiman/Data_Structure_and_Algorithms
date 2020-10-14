c5de65f8d4fd30087b585a8be7d391284a4acc48
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2020-10-14 17:07:27 +0530

    Update Paranthesis Matching Using Stack.cpp

--- Stack/Paranthesis Matching Using Stack.cpp
+++ Stack/Paranthesis Matching Using Stack.cpp
@@ -1,6 +1,21 @@
 using namespace std;
 #include<iostream>
 
+int top = -1, n = 100;
+char stack[100];
+void push(int x)
+{
+	if(top == n-1)
+	{
+		cout <<"Stack Overflow";
+	}
+	else
+	{
+		top++;
+		stack[top] = x;
+	}
+}
+
 int main()
 {
 	char c[30];
