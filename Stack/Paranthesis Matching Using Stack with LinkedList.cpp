40f012ae2c463a30218c741a5fa5864b0397993f
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2020-10-15 13:30:48 +0530

    Update Paranthesis Matching Using Stack with LinkedList.cpp

--- Stack/Paranthesis Matching Using Stack with LinkedList.cpp
+++ Stack/Paranthesis Matching Using Stack with LinkedList.cpp
@@ -5,3 +5,21 @@ struct node{
 	int data;
 	struct node *next;
 };
+
+struct node *top = NULL;
+
+void push(int data)
+{
+	struct node *newnode = (struct node *) new node();
+	if(newnode == NULL)
+	{
+		cout << "Stack is Full";
+	}
+	else
+	{
+		newnode -> data = data;
+		newnode -> next = top;
+		top = newnode;
+	}
+}
+
