abf65d749d598e736ab6665ab5ff0c2f45781615
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2020-11-01 21:54:30 +0530

    Update Queue Operation.cpp

--- Queue/Queue Operation.cpp
+++ Queue/Queue Operation.cpp
@@ -2,3 +2,21 @@ using namespace std;
 #include<iostream>
 
 int queue[100], n = 100, front = -1, rear = -1;
+
+void insert(int data)
+{
+	if(rear == n - 1)
+	{
+		cout << "Queue is Full" << "\n";	
+	}	
+	else
+	{
+		if(front == -1)
+		{
+			front = 0;
+		}
+		rear++;
+		queue[rear] = data;
+		cout << "Insertion Done ..................." << "\n";
+	}
+}
