3f217d4a45c76a48582b3d8fa336135558333ca7
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2020-11-02 23:09:10 +0530

    Update Queue Operation Using Linked List.cpp

--- Queue/Queue Operation Using Linked List.cpp
+++ Queue/Queue Operation Using Linked List.cpp
@@ -9,3 +9,24 @@ struct node{
 };
 
 struct node *head = NULL;
+
+void insert(int data)
+{
+	struct node *newnode = (struct node *) new node();
+	
+	if(newnode == NULL)
+	{
+		cout << "Queue is Empty";
+	}
+	else
+	{
+		if(newnode -> fornt == -1)
+		{
+			newnode -> fornt = 0;
+		}
+		cout << "Insertion Done...........";
+		newnode -> rear = newnode -> rear + 1;
+		newnode -> data =  data;
+			
+	}
+}
