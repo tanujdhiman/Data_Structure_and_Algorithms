6bc6114c78cb897ab58801f19f7147dc4783c70b
Tanuj Dhiman <56601466+tanujdhiman@users.noreply.github.com>
2020-11-06 22:10:47 +0530

    Update Circular Queue Using Array.cpp

--- Queue/Circular Queue Using Array.cpp
+++ Queue/Circular Queue Using Array.cpp
@@ -3,6 +3,21 @@ using namespace std;
 
 int queue[100], n = 100, front = 0, rear = 0;
 
+void insert(int data)
+{
+	if(((rear+1) % n) == front)
+	{
+		cout << "Queue is Full" << "\n";	
+	}	
+	else
+	{
+		rear = (rear+1) % n;
+		queue[rear] = data;
+		cout << "Insertion Done ..................." << "\n";
+	}
+}
+
+
 int main()
 {
   return 0;
